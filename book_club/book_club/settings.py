8194f835a37a087203d38c20a9188c08d9b6daa9
LaryR2007 <laryrenderos2007@gmail..com>
2025-04-03 23:10:05 -0400

    done, just need css

--- book_club/book_club/settings.py
+++ book_club/book_club/settings.py
@@ -118,11 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),  # Custom directory for static files
 ]
 
 # Default primary key field type
